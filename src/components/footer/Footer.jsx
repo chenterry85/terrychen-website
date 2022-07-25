2862c5733d1c6a7f01521256081642c95a2665de
Terry Chen <chenterry85@gmail.com>
2022-07-24 23:09:45 -0400

    Add footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,27 @@ import {IoLogoInstagram} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-     
+      <a href="#" className='footer__logo'>TERRY'S</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li> 
+        <li><a href='#about'>About</a></li> 
+        <li><a href='#experience'>Experience</a></li> 
+        <li><a href='#services'>Services</a></li> 
+        <li><a href='#testimonials'>Testimonials</a></li> 
+        <li><a href='#contact'>Contact</a></li> 
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://facebook.com"><RiFacebookLine/></a>
+        <a href="https://instagram.com"><IoLogoInstagram/></a>
+        <a href="https://twitter.com"><RiTwitterLine/></a>
+
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; EGATOR tutorials. All rights reserved.</small>
+      </div>
     </footer>
   )
 }
